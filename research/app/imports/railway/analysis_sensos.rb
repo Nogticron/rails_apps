76c0d6ef29d4b5aded5d0a1127f84197ed732531
野口大輔 <einstein.mastermind11235@gmail.com>
2019-11-11 03:50:32 +0900

    修正

--- research/app/imports/railway/analysis_sensos.rb
+++ research/app/imports/railway/analysis_sensos.rb
@@ -92,17 +92,15 @@ class Railway::AnalysisSensos
       person.update(am_departure_time2: person.am_arrival_time2 + 120)
     end
 
-    # am_st2出発からam_st3到着までの時間を設定する
     if via_station_num > 0
+      # am_st2出発からam_st3到着までの時間を設定する
       start_st_idx = @station_list.pluck(:name).index(person.am_st2)
       goal_st_idx = @station_list.pluck(:name).index(person.am_st3)
       if start_st_idx && goal_st_idx
         ride_time = @between_time[start_st_idx][goal_st_idx].to_i * 60
         person.am_arrival_time3 = person.am_departure_time2 + ride_time
       end
-    end
 
-    if via_station_num > 1
       # am_st3滞留の時間を設定する
       if person.st3_id
         stay_time = Station.find(person.st3_id).transfer_time
@@ -110,7 +108,9 @@ class Railway::AnalysisSensos
       else
         person.update(am_departure_time3: person.am_arrival_time3 + 120)
       end
+    end
 
+    if via_station_num > 1
       # am_st3出発からam_st4到着までの時間を設定する
       start_st_idx = @station_list.pluck(:name).index(person.am_st3)
       goal_st_idx = @station_list.pluck(:name).index(person.am_st4)
@@ -118,9 +118,7 @@ class Railway::AnalysisSensos
         ride_time = @between_time[start_st_idx][goal_st_idx].to_i * 60
         person.am_arrival_time4 = person.am_departure_time3 + ride_time
       end
-    end
 
-    if via_station_num > 2
       # am_st4滞留の時間を設定する
       if person.st4_id
         stay_time = Station.find(person.st4_id).transfer_time
@@ -128,7 +126,9 @@ class Railway::AnalysisSensos
       else
         person.update(am_departure_time4: person.am_arrival_time4 + 120)
       end
+    end
 
+    if via_station_num > 2
       # am_st4出発からam_st5到着までの時間を設定する
       start_st_idx = @station_list.pluck(:name).index(person.am_st4)
       goal_st_idx = @station_list.pluck(:name).index(person.am_st5)
@@ -136,9 +136,7 @@ class Railway::AnalysisSensos
         ride_time = @between_time[start_st_idx][goal_st_idx].to_i * 60
         person.am_arrival_time5 = person.am_departure_time4 + ride_time
       end
-    end
 
-    if via_station_num > 3
       # am_st5滞留の時間を設定する
       if person.st5_id
         stay_time = Station.find(person.st5_id).transfer_time
@@ -146,7 +144,9 @@ class Railway::AnalysisSensos
       else
         person.update(am_departure_time5: person.am_arrival_time5 + 120)
       end
+    end
 
+    if via_station_num > 3
       # am_st5出発からam_st6到着までの時間を設定する
       start_st_idx = @station_list.pluck(:name).index(person.am_st5)
       goal_st_idx = @station_list.pluck(:name).index(person.am_st6)
@@ -154,9 +154,7 @@ class Railway::AnalysisSensos
         ride_time = @between_time[start_st_idx][goal_st_idx].to_i * 60
         person.am_arrival_time6 = person.am_departure_time5 + ride_time
       end
-    end
 
-    if via_station_num > 4
       # am_st6滞留の時間を設定する
       if person.st6_id
         stay_time = Station.find(person.st6_id).transfer_time
@@ -164,7 +162,9 @@ class Railway::AnalysisSensos
       else
         person.update(am_departure_time6: person.am_arrival_time6 + 120)
       end
+    end
 
+    if via_station_num > 4
       # am_st6出発からam_st7到着までの時間を設定する
       start_st_idx = @station_list.pluck(:name).index(person.am_st6)
       goal_st_idx = @station_list.pluck(:name).index(person.am_st7)
@@ -172,9 +172,7 @@ class Railway::AnalysisSensos
         ride_time = @between_time[start_st_idx][goal_st_idx].to_i * 60
         person.am_arrival_time7 = person.am_departure_time6 + ride_time
       end
-    end
 
-    if via_station_num > 5
       # am_st7滞留の時間を設定する
       if person.st7_id
         stay_time = Station.find(person.st7_id).transfer_time
@@ -182,7 +180,9 @@ class Railway::AnalysisSensos
       else
         person.update(am_departure_time7: person.am_arrival_time7 + 120)
       end
+    end
 
+    if via_station_num > 5
       # am_st7出発からam_st8到着までの時間を設定する
       start_st_idx = @station_list.pluck(:name).index(person.am_st7)
       goal_st_idx = @station_list.pluck(:name).index(person.am_st8)
@@ -204,36 +204,48 @@ class Railway::AnalysisSensos
   end
 
   def self.default_set_via_station_time(person, via_station_num)
+    # 駅滞留時間は150秒統一
+    stay_time = 150
     ride_time = person.am_arriving_time - person.am_departure_time1
-    between_time = ride_time / (via_station_num + 1)
+    between_time = (ride_time / (via_station_num + 1) ) - stay_time
+
+    person.am_arrival_time1 = person.am_departure_time1 - stay_time
 
     if via_station_num >= 0
-      person.update(am_arrival_time2: person.am_departure_time1 + between_time)
+      person.am_arrival_time2 = person.am_departure_time1 + between_time
+      person.am_departure_time2 = person.am_arrival_time2 + stay_time
     end
 
     if via_station_num >= 1
-      person.update(am_arrival_time3: person.am_arrival_time2 + between_time)
+      person.am_arrival_time3 = person.am_departure_time2 + between_time
+      person.am_departure_time3 = person.am_arrival_time3 + stay_time
     end
 
     if via_station_num >= 2
-      person.am_arrival_time3 + between_time
-      person.update(am_arrival_time4: person.am_arrival_time3 + between_time)
+      person.am_arrival_time4 = person.am_departure_time3 + between_time
+      person.am_departure_time4 = person.am_arrival_time4 + stay_time
     end
 
     if via_station_num >= 3
-      person.update(am_arrival_time5: person.am_arrival_time4 + between_time)
+      person.am_arrival_time5 = person.am_departure_time4 + between_time
+      person.am_departure_time5 = person.am_arrival_time5 + stay_time
     end
 
     if via_station_num >= 4
-      person.update(am_arrival_time6: person.am_arrival_time5 + between_time)
+      person.am_arrival_time6 = person.am_departure_time5 + between_time
+      person.am_departure_time6 = person.am_arrival_time6 + stay_time
     end
 
     if via_station_num >= 5
-      person.update(am_arrival_time7: person.am_arrival_time6 + between_time)
+      person.am_arrival_time7 = person.am_departure_time6 + between_time
+      person.am_departure_time7 = person.am_arrival_time7 + stay_time
     end
 
     if via_station_num >= 6
-      person.update(am_arrival_time8: person.am_arrival_time7 + between_time)
+      person.am_arrival_time8 = person.am_departure_time7 + between_time
+      person.am_departure_time8 = person.am_arrival_time8 + stay_time
     end
+
+    person.save!
   end
 end
